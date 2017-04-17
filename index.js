b85f4d6ed1de5f357c055c508fb18c738c7ae620
kj <kristoferjoseph@users.noreply.github.com>
2017-04-17 16:05:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function hashswitch (actions, defaultCallback){
   actions && Object.keys(actions).forEach(function (action) {
     ('string' === typeof action && 'function' === typeof actions[action]) ?
     (hash[action] = actions[action]) :
-    console.log(action + ' should be a string and ' + actions[action] + ' should be a function.')
+    console.error(action + ' should be a string and ' + actions[action] + ' should be a function.')
   })
   return function exectue (action) {
    'string' !== typeof action && console.error(action + ' should be a string')
